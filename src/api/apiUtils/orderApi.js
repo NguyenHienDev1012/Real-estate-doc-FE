bd1dea08b42a1d0b17db3e36b78a70d1d093de8d
Nguyen Hien <nguyenthanhhien.itnlu@gmail.com>
2025-03-29 01:24:08 +0700

    fix#123: refactor code
    fix#123: refactor code

--- src/api/apiUtils/orderApi.js
+++ src/api/apiUtils/orderApi.js
@@ -1,31 +1,5 @@
 import axiosClient from '../axiosClient';
 
-const contractApi = {
-  getAll: (params) => {
-    const url = 'contracts-all';
-    return axiosClient.get(url, params);
-  },
-  
-  getOne: (params) => {
-    const url = `contract/${params}`;
-    return axiosClient.get(url);
-  },
-  upload: (data) => {
-    const url = 'contract';
-    return axiosClient.post(url, data);
-  },
-  edit: (data) => {
-    const url = 'save-contract';
-    return axiosClient.post(url, data);
-  },
-  delete: (params) => {
-    const url = `contract/${params}`;
-    return axiosClient.delete(url);
-  },
- 
-  searchByName: (params) => {
-    const url = 'contracts-search';
-    return axiosClient.get(url, { params });
-  },
+const Api = {
 };
 export default contractApi;
